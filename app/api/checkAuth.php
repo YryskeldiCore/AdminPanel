e6fc91248dfbd7eb62b4d667ab51fd5b8132af23
Yrys <yrys.amanturov@mail.ru>
2021-10-14 21:44:19 +0600

    a little fix with checkAuth file

--- app/api/checkAuth.php
+++ app/api/checkAuth.php
@@ -2,7 +2,7 @@
 session_start();
 
 if($_SESSION["auth"] == true){
-    echo json_encode(array("auth" => true))
+    echo json_encode(array("auth" => true));
 } else {
-    echo json_encode(array("auth" => false))
+    echo json_encode(array("auth" => false));
 }
